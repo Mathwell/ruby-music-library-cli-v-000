0a8306139c1525ea8b55d3a30d45d98701d40b06
Olena Ageyeva <eageeva@hotmail.com>
2017-11-21 04:39:49 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Concerns
   module Findable
     def find_by_name(name)
-      all.detect{ |a| a.name == name }.class
+      all.detect{ |a| a.name == name }
     end
 
     def find_or_create_by_name(name)
