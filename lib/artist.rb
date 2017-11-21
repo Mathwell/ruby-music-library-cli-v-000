211eb6d1e0a4a9f6f1b9491895bf145e8def4b7e
Olena Ageyeva <eageeva@hotmail.com>
2017-11-21 04:37:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,9 +14,19 @@ class Artist
     @@all
   end
 
+  def destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all<<self
+  end
+
+
   def self.create(name)
-    self.new(name).save
-    self
+    artist = new(name)
+     artist.save
+     artist
   end
 
   def add_song(song)
