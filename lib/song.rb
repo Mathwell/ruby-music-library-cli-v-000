b8ca89d00fa4b37bd008b49cbe46b74046203bb9
Olena Ageyeva <eageeva@hotmail.com>
2017-11-21 04:50:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,13 +53,7 @@ end
     all<<self
   end
 
-  def self.find_by_name(name)
-    all.detect{ |s| s.name == name }
-  end
 
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)
-  end
 
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
